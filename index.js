c75fdf499a27f671bfddf943b983cb3ec2590476
DinoscapeProgramming <emailfromdinoscape@gmail.com>
2025-09-11 19:17:20 +0200

    chore(deps): use correct surrealdb dependency

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Surreal = require("surrealdb.js").default;
+const Surreal = require("surrealdb").default;
 
 class FluidDB {
   constructor({ url = "http://127.0.0.1:8000/rpc", namespace = "test", database = "test" }) {
